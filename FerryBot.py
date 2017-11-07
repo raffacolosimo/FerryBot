b9ae131f1e66b7cf477ae255b6c3f9b3e8eae930
Raffaello Colosimo <raffa.colosimo@gmail.com>
2017-11-07 22:34:36 +0100

    aggiunti countdown e messaggi durante foto e video

--- FerryBot.py
+++ FerryBot.py
@@ -28,6 +28,7 @@ class FerryBot(telepot.Bot):
         # Set some camera options if needed
         # self.camera.vflip = True
         # self.camera.hflip = True
+        return
         
     """ gestisce i messaggi in arrivo """
     def handle(self, msg):
@@ -44,20 +45,32 @@ class FerryBot(telepot.Bot):
                 self.sendMessage(chat_id, 'Ciao!')
 
             elif command == '/foto':
-                self.sendMessage(chat_id, "Adesso ti mando una foto")
+                self.sendMessage(chat_id, "Adesso ti faccio una foto.")
+                self.sendMessage(chat_id, "Sei pronto? Guarda il display.")
                 self.DisplayOff()
                 # cancella immagine precedente
                 try:
                     os.remove('image.jpg')
                 except OSError:
                     pass
+                    
                 self.camera.resolution = (1920, 1080)
+                time.sleep(0.5)
+                self.countdown()
+                self.clic()
+                time.sleep(0.2)
                 self.camera.capture('image.jpg')
+                time.sleep(0.8)
+                self.DisplayOff()
+                
+                self.sendMessage(chat_id, "Fatto!")
+                self.sendMessage(chat_id, "Attendi un attimo per il caricamento...")
                 f = open('image.jpg', 'rb')
                 self.sendPhoto(chat_id, f)                
 
             elif command == '/video':
-                self.sendMessage(chat_id, "Faccio un filmato di 5 secondi. Attendi un attimo per il caricamento...")
+                self.sendMessage(chat_id, "Ora ti faccio un filmato di 5 secondi.")
+                self.sendMessage(chat_id, "Sei pronto? Guarda il display.")
                 self.DisplayOff()
                 # cancella video precedente
                 try:
@@ -68,10 +81,20 @@ class FerryBot(telepot.Bot):
                     os.remove('video.mp4')
                 except OSError:
                     pass
+                    
                 self.camera.resolution = (1280, 720)
+                time.sleep(0.5)
+                self.countdown()
                 self.camera.start_recording('video.h264')
-                self.camera.wait_recording(5)
+                self.recblink()
+                #self.camera.wait_recording(5)
                 self.camera.stop_recording()
+                self.endrec()
+                time.sleep(1)
+                self.DisplayOff()
+                self.sendMessage(chat_id, "Fatto!")
+                self.sendMessage(chat_id, "Attendi qualche secondo per il caricamento...")
+
                 cmd = ['MP4Box', '-add', 'video.h264', 'video.mp4']
                 try:
                     check_call(cmd)
@@ -113,41 +136,84 @@ class FerryBot(telepot.Bot):
             self.invalid_users_count += 1
             self.sendMessage(chat_id, 'Io non ti conosco!')
             self.sendMessage(chat_id, sender) # invia l'ID del chiamante per poterlo inserire fra gli utenti registrati
-    
+        return
+        
     def clock(self):
         scrollphathd.set_brightness(0.3)
         scrollphathd.rotate(270)
         scrollphathd.clear()
         # Ore
-        scrollphathd.write_string(
-            time.strftime("%H"),
-            x=0,
-            y=0,
-            font=font5x5)
+        scrollphathd.write_string(time.strftime("%H"),x=0,y=0,font=font5x5)
         # Minuti
-        scrollphathd.write_string(
-            time.strftime("%M"),
-            x=0,
-            y=6,
-            font=font5x5)
+        scrollphathd.write_string(time.strftime("%M"),x=0,y=6,font=font5x5)
         # Secondi
-        scrollphathd.write_string(
-            time.strftime("%S"),
-            x=0,
-            y=12,
-            font=font5x5)
+        scrollphathd.write_string(time.strftime("%S"),x=0,y=12,font=font5x5)
         scrollphathd.show()
+        return
 
     def DisplayScroll(self):
         scrollphathd.show()
         scrollphathd.scroll()
+        return
 
     def DisplayOff(self):
         self.clockDisplay = False
         self.textMessageDisplay = False
         scrollphathd.clear()
         scrollphathd.show()
+        return
 
+    def countdown(self):    
+        scrollphathd.set_brightness(0.3)
+        scrollphathd.rotate(0)
+        # 3 - 2 - 1
+        scrollphathd.clear()
+        scrollphathd.write_string('3', x=6, y=0, font=font5x7)
+        scrollphathd.show()
+        time.sleep(1)
+        scrollphathd.clear()
+        scrollphathd.write_string('2', x=6, y=0, font=font5x7)
+        scrollphathd.show()
+        time.sleep(1)
+        scrollphathd.clear()
+        scrollphathd.write_string('1', x=7, y=0, font=font5x7)
+        scrollphathd.show()
+        time.sleep(1)
+        return
+
+    def clic(self):    
+        scrollphathd.set_brightness(0.4)
+        scrollphathd.rotate(0)
+        scrollphathd.clear()
+        scrollphathd.write_string('c', x=0, y=0)
+        scrollphathd.write_string('l', x=5, y=0)
+        scrollphathd.write_string('i', x=9, y=0)
+        scrollphathd.write_string('c', x=12, y=0)
+        scrollphathd.show()
+        return
+
+    def recblink(self):    
+        scrollphathd.set_brightness(0.15)
+        scrollphathd.rotate(0)
+        for i in range(5):
+            scrollphathd.clear()
+            scrollphathd.write_string('REC', x=0, y=0)
+            scrollphathd.show()
+            time.sleep(0.7)
+            scrollphathd.clear()
+            scrollphathd.show()
+            time.sleep(0.3)
+        return
+
+    def endrec(self):
+        scrollphathd.set_brightness(0.3)
+        scrollphathd.rotate(0)
+        scrollphathd.clear()
+        scrollphathd.write_string('ok', x=3, y=0)
+        scrollphathd.show()
+        return
+
+        
 #start the app/webserver
 if __name__ == "__main__":
     bot = FerryBot('407932832:AAHQhAfKV7oIVIcGQ_L4Q0H_iLT4NV0WLx8')
