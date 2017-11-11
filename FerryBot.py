aa6160ca93bf75c11c19f99e864009512db75be8
Raffaello Colosimo <raffa.colosimo@gmail.com>
2017-11-11 16:42:40 +0100

    Aggiunti messaggi funzione per ciao e foto. modificato il clic e il countdown. aggiunto scroll non in loop

--- FerryBot.py
+++ FerryBot.py
@@ -1,19 +1,13 @@
-import sys
-import os
-import telepot
-import datetime
-import time
-import picamera
-from subprocess import check_call, CalledProcessError
-import scrollphathd
-from scrollphathd.fonts import font5x5, font5x7
+import os           # per cancellare i file
+import telepot      # per gestire telegram
+import time         # per gestire il tempo
+import picamera     # per la telecamera
+from subprocess import check_call, CalledProcessError # per la conversione MP4
+import scrollphathd # per il display scroll pHat HD
+from scrollphathd.fonts import font5x5, font5x7 # per i font del display
 
-
-"""
-Ctrl-C per uscire.
-"""
- 
-id_a = [45170836,11111111,22222222] # lista ID autorizzati
+# lista ID telegram autorizzati
+id_a = [45170836,11111111,22222222] 
  
 class FerryBot(telepot.Bot):
     def __init__(self, access_token): # il codice telegram e' passato come argomento
@@ -25,13 +19,13 @@ class FerryBot(telepot.Bot):
         self.clockDisplay = False               # mostra l'orologio sul display
         self.textMessageDisplay = False         # mostra il messaggio di testo sul display
         self.pulisciDisplay = False             # spegne il display (cancella)
-        # Set some camera options if needed
+        self.LEDscrollLeft = 0                   # numero di pixel rimasti da far scorrere 
+        # Eventuali opzioni per la telecamera
         # self.camera.vflip = True
         # self.camera.hflip = True
         return
-        
-    """ gestisce i messaggi in arrivo """
-    def handle(self, msg):
+
+    def handle(self, msg):         #gestisce i messaggi in arrivo
         chat_id = msg['from']['id']
         user_name = "%s %s" % (msg['from']['first_name'], msg['from']['last_name'])
         command = msg['text']
@@ -43,26 +37,12 @@ class FerryBot(telepot.Bot):
         if sender in id_a:
             if   command == '/ciao':
                 self.sendMessage(chat_id, 'Ciao!')
+                self.LEDmessage('ciao') # messaggio LED, non in loop
 
             elif command == '/foto':
                 self.sendMessage(chat_id, "Adesso ti faccio una foto.")
                 self.sendMessage(chat_id, "Sei pronto? Guarda il display.")
-                self.DisplayOff()
-                # cancella immagine precedente
-                try:
-                    os.remove('image.jpg')
-                except OSError:
-                    pass
-                    
-                self.camera.resolution = (1920, 1080)
-                time.sleep(0.5)
-                self.countdown()
-                self.clic()
-                time.sleep(0.2)
-                self.camera.capture('image.jpg')
-                time.sleep(0.8)
-                self.DisplayOff()
-                
+                self.takePhoto()
                 self.sendMessage(chat_id, "Fatto!")
                 self.sendMessage(chat_id, "Attendi un attimo per il caricamento...")
                 f = open('image.jpg', 'rb')
@@ -120,25 +100,34 @@ class FerryBot(telepot.Bot):
                 
             else:
                 self.sendMessage(chat_id, "Scrivo il tuo messaggio sul display")
-                self.DisplayOff()
-                
-                scrollphathd.rotate(0)
-                scrollphathd.clear()
-                scrollphathd.set_brightness(0.7)
                 textMessage = command + "____"
-                scrollphathd.write_string(textMessage, x=0, y=0, font=font5x7)
-                scrollphathd.show()
-                time.sleep(0.4)
-                self.clockDisplay = False
-                self.textMessageDisplay = True
-
+                self.LCDmessage(textMessage)
+                
         else: # utente non nella lista degli autorizzati
             self.invalid_users_count += 1
             self.sendMessage(chat_id, 'Io non ti conosco!')
             self.sendMessage(chat_id, sender) # invia l'ID del chiamante per poterlo inserire fra gli utenti registrati
         return
         
-    def clock(self):
+    def LEDmessage(self, txtMessage, loop=False):
+        self.DisplayOff()
+        scrollphathd.rotate(0)
+        scrollphathd.clear()
+        scrollphathd.set_brightness(0.5)
+        scrollphathd.write_string(txtMessage, x=18, y=0, font=font5x7)
+        if loop == True:
+            self.textMessageDisplay = True
+            self.LEDscrollLeft = 0
+        else:
+            self.textMessageDisplay = False
+            self.LEDscrollLeft = scrollphathd.get_buffer_shape()[0]
+            #print self.LEDscrollLeft
+            #print self.scrollphathd.get_buffer_shape()
+        scrollphathd.show()
+        self.clockDisplay = False
+            
+
+    def clockVert(self):
         scrollphathd.set_brightness(0.3)
         scrollphathd.rotate(270)
         scrollphathd.clear()
@@ -151,18 +140,49 @@ class FerryBot(telepot.Bot):
         scrollphathd.show()
         return
 
-    def DisplayScroll(self):
-        scrollphathd.show()
-        scrollphathd.scroll()
-        return
-
-    def DisplayOff(self):
-        self.clockDisplay = False
-        self.textMessageDisplay = False
+    def clockHor(self):
+        scrollphathd.set_brightness(0.5)
+        scrollphathd.rotate(0)
         scrollphathd.clear()
+        BRIGHTNESS = 0.3
+        # prende il resto della divisione per 60 (= secondi) e lo convert nel campo 0.0-15.0
+        seconds_progress = 15 * ((time.time() % 60) / 59.0) # ogni pixel sono 4 secondi
+        # barra dei secondi a luminosita' variabile
+        for y in range(15): # 15 = numero di pixel
+            # For each pixel, we figure out its brightness by
+            # seeing how much of "seconds_progress" is left to draw
+            # If it's greater than 1 (full brightness) then we just display 1.
+            current_pixel = min(seconds_progress, 1)
+            # Multiply the pixel brightness (0.0 to 1.0) by our global brightness value
+            scrollphathd.set_pixel(y + 1, 6, current_pixel * BRIGHTNESS)
+            # Subtract 1 now we've drawn that pixel
+            seconds_progress -= 1
+            # If we reach or pass 0, there are no more pixels left to draw
+            if seconds_progress <= 0:
+                break
+        # Display the time (HH:MM) in a 5x5 pixel font
+        scrollphathd.write_string(
+            time.strftime("%H:%M"),
+            x=0, # Align to the left of the buffer
+            y=0, # Align to the top of the buffer
+            font=font5x5, # Use the font5x5 font we imported above
+            brightness=BRIGHTNESS # Use our global brightness value
+        )
+        # int(time.time()) % 2 will tick between 0 and 1 every second.
+        # We can use this fact to clear the ":" and cause it to blink on/off
+        # every other second, like a digital clock.
+        # To do this we clear a rectangle 8 pixels along, 0 down,
+        # that's 1 pixel wide and 5 pixels tall.
+        if int(time.time()) % 2 == 0:
+            scrollphathd.clear_rect(8, 0, 1, 5)
+        # Display our time and sleep a bit. Using 1 second in time.sleep
+        # is not recommended, since you might get quite far out of phase
+        # with the passing of real wall-time seconds and it'll look weird!
+        #
+        # 1/10th of a second is accurate enough for a simple clock though :D
         scrollphathd.show()
         return
-
+        
     def countdown(self):    
         scrollphathd.set_brightness(0.3)
         scrollphathd.rotate(0)
@@ -170,25 +190,62 @@ class FerryBot(telepot.Bot):
         scrollphathd.clear()
         scrollphathd.write_string('3', x=6, y=0, font=font5x7)
         scrollphathd.show()
-        time.sleep(1)
+        time.sleep(0.75)
+        scrollphathd.clear()
+        scrollphathd.show()
+        time.sleep(0.25)
         scrollphathd.clear()
         scrollphathd.write_string('2', x=6, y=0, font=font5x7)
         scrollphathd.show()
-        time.sleep(1)
+        time.sleep(0.75)
+        scrollphathd.clear()
+        scrollphathd.show()
+        time.sleep(0.25)
         scrollphathd.clear()
         scrollphathd.write_string('1', x=7, y=0, font=font5x7)
         scrollphathd.show()
-        time.sleep(1)
+        time.sleep(0.75)
+        scrollphathd.clear()
+        scrollphathd.show()
+        time.sleep(0.25)
         return
 
-    def clic(self):    
-        scrollphathd.set_brightness(0.4)
+    def takePhoto(self):    
+        self.DisplayOff()
         scrollphathd.rotate(0)
+        scrollphathd.set_brightness(0.25)
+        #scorre scritta foto
         scrollphathd.clear()
-        scrollphathd.write_string('c', x=0, y=0)
-        scrollphathd.write_string('l', x=5, y=0)
-        scrollphathd.write_string('i', x=9, y=0)
-        scrollphathd.write_string('c', x=12, y=0)
+        scrollphathd.write_string('foto', x=18, y=0, font=font5x7)
+        scrollphathd.show()
+        #print 'scritta foto'
+        buffL=scrollphathd.get_buffer_shape()[0]
+        #print buffL
+        time.sleep(1)
+        for i in range(buffL+1):
+            scrollphathd.show()
+            scrollphathd.scroll()
+            time.sleep(0.020)
+        # cancella immagine precedente
+        try:
+            os.remove('image.jpg')
+        except OSError:
+            pass
+        self.camera.resolution = (1920, 1080)
+        self.countdown()
+        # clic
+        scrollphathd.clear()
+        scrollphathd.set_brightness(0.7)
+        scrollphathd.fill(1, x=0, y=0, width=17, height=7)
+        #scrollphathd.write_string('c', x=0, y=0)
+        #scrollphathd.write_string('l', x=5, y=0)
+        #scrollphathd.write_string('i', x=9, y=0)
+        #scrollphathd.write_string('c', x=12, y=0)
+        scrollphathd.show()
+        #scatta la foto
+        time.sleep(0.01)
+        self.camera.capture('image.jpg')
+        scrollphathd.clear()
         scrollphathd.show()
         return
 
@@ -213,20 +270,46 @@ class FerryBot(telepot.Bot):
         scrollphathd.show()
         return
 
-        
+    def DisplayScrollLoop(self):
+        scrollphathd.show()
+        scrollphathd.scroll()
+        return
+    def DisplayScrollOnce(self):
+        if self.LEDscrollLeft > 0:
+            self.LEDscrollLeft -= 1 # decrementa il contatore dei pixel rimasti da far scorrere
+            scrollphathd.show()
+            scrollphathd.scroll()
+        if self.LEDscrollLeft ==0:
+            scrollphathd.show()
+            scrollphathd.clear()
+        return
+
+    def DisplayOff(self):
+        self.clockDisplay = False
+        self.textMessageDisplay = False
+        self.LEDscrollLeft = 0
+        scrollphathd.clear()
+        scrollphathd.show()
+        return
+
+    def run(self):
+        # gestisce le visualizzazioni 
+        while True:
+            time.sleep(0.05)
+            if bot.pulisciDisplay: # se e' richiesto lo spegnimento del display lo effettua
+                bot.DisplayOff()
+                bot.pulisciDisplay = False
+
+            if self.clockDisplay: # se l'orologio e' attivo lo aggiorna
+                self.clockHor()
+            elif self.textMessageDisplay: # se e' attivo lo scroll a nastro continuo del display, lo effettua
+                self.DisplayScrollLoop()
+            elif self.LEDscrollLeft > 0: # se e' attivo lo scroll a singolo messaggio, lo effettua
+                self.DisplayScrollOnce()
+                
 #start the app/webserver
 if __name__ == "__main__":
     bot = FerryBot('407932832:AAHQhAfKV7oIVIcGQ_L4Q0H_iLT4NV0WLx8')
     bot.message_loop(bot.handle)
-    print 'Ok, Sono in ascolto...'
-    # ciclo infinito
-    while True:
-        time.sleep(0.05)
-        if bot.clockDisplay:
-            bot.clock()
-        elif bot.textMessageDisplay:
-            bot.DisplayScroll()
-        
-        if bot.pulisciDisplay:
-            bot.DisplayOff()
-            bot.pulisciDisplay = False
+    print 'Ok, FerryBot in ascolto...'
+    bot.run() # gestisce le visualizzazioni LED
